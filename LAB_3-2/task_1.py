658a96456db456205c654add61d45a52d565752b
AliyaShigapova <149310327+AliyaShigapova@users.noreply.github.com>
2024-02-12 01:04:28 +0300

    Add files via upload

---
+++ LAB_3-2/task_1.py
@@ -0,0 +1,57 @@
+class Book:
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга: {self.name}, автор: {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name='{self.name}', author='{self.author}')"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value: int):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть целым положительным числом")
+        self._pages = value
+
+    def __str__(self):
+        return f"{super().__str__()} ({self.pages} стр.)"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value: float):
+        if not isinstance(value, float) or value <= 0:
+            raise ValueError("Продолжительность должна быть числом с плавающей запятой > 0")
+        self._duration = value
+
+    def __str__(self):
+        return f"{super().__str__()} ({self.duration:.2f} часов)"
+
